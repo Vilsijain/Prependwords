9174a664216068ff6bb868ec0f371890f1577b7e
Vilsijain <93503754+Vilsijain@users.noreply.github.com>
2022-06-15 22:31:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,31 +6,13 @@ from flask import Flask,jsonify
 app = Flask(__name__)
 
 @app.route('/<string:words>')
-def append(words):
+def prepend(words):
     result=[]
     str2 = ['ve', 'bright', 'toffee', 'code', 'community', 'dev', 'eat', 'drink', 'repeat']
     for i in random.sample(str2,5):
         strres=i+words
-        result.append(strres)
+        result.prepend(strres)
     return jsonify(result)
 if __name__ == '__main__':
     port = os.getenv('PORT',None) or 80
     app.run(host='0.0.0.0',port=port,debug=True)
-# import os
-# import random
-# from flask import Flask,jsonify
-
-
-# app = Flask(__name__)
-
-# @app.route('/<string:words>')
-# def prepend(words):
-#     result=[]
-#     str2 = ['ve', 'bright', 'toffee', 'code', 'community', 'dev', 'eat', 'drink', 'repeat']
-#     for i in random.sample(str2,5):
-#         strfin=i+words
-#         result.prepend(strfin) 
-#     return jsonify(result)
-# if __name__ == '__main__':
-#     port = os.getenv('PORT',None) or 80
-#     app.run(host='0.0.0.0',port=port,debug=True)
